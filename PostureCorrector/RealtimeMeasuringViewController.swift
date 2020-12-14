b496ff8e870e8111e4d7e03da10bc38de72c3aa6
Woozzang <woozzaang@gmail.com>
2020-12-14 17:28:15 +0900

    Add correctRate, badRate properties

--- PostureCorrector/RealtimeMeasuringViewController.swift
+++ PostureCorrector/RealtimeMeasuringViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class RealtimeMeasuringViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  
+  @IBOutlet weak var correctRateLabel: UILabel!
+  @IBOutlet weak var badRateLabel: UILabel!
+  
+  private var resultList: [Result] = [] {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Called when new element added
+    didSet {
+      correctRateLabel.text = "\(correctRate) %"
+      badRateLabel.text = "\(badRate) %"
     }
-    */
+  }
 
+  override func viewDidLoad() {
+      super.viewDidLoad()
+    
+      // Do any additional setup after loading the view.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    resultList.removeAll()
+  }
+  
+  @IBAction func stopMeasuring(_ sender: UIButton) {
+    
+  }
+}
+
+extension RealtimeMeasuringViewController {
+  var correctRate: Double {
+    get {
+      (Double(resultList.filter{$0.postureType == Result.PostureType.Correct}.count) / Double( resultList.count)) * 100
+    }
+  }
+  
+  var badRate: Double {
+    get {
+      100 - correctRate
+    }
+  }
+  
 }
